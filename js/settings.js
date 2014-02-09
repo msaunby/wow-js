38f3a7d31725962cc9e811d5b9590f64b23c6d9e
Michael Saunby <mike@saunby.net>
2014-02-09 14:25:21 +0000

    Now uploads pressure data to Met Office WOW service. Converts to Mean Sea Level Pressure - requires altitude of station.

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,23 @@
 // settings.js
 
+
+// Upload interval in seconds.
+exports.interval = 180;
+
+// XIVELY
+//
 // Your Xively feed ID.
 exports.id='111111111';
-
 // Generate an API key at the Xively website.  A read only key is probably best here - just in case.  
 exports.key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+// WOW
+//
+exports.wow=true;
+// Your site info..
+exports.siteid='222222222';
+exports.siteAuthenticationKey='123456';
+exports.altitude=0.0;
+
+
+
--- js/xively_to_wow.js
+++ js/xively_to_wow.js
@@ -3,11 +3,13 @@
 //
 
 https = require('https');
+http = require('http');
+querystring = require('querystring');
 var settings = require('./settings.js');
 
-function dateUTC()
+// Format date and time as WOW likes it (UTC)
+function dateUTC( now )
 {
-    var now=new Date();
     var YYYY=now.getUTCFullYear();
     var MM=now.getUTCMonth()+1;
     MM=MM < 10 ? "0"+MM : MM;
@@ -22,7 +24,60 @@ function dateUTC()
     return ""+YYYY+"-"+MM+"-"+DD+" "+hh+":"+mm+":"+ss;
 }
 
-xivelyURL='https://api.xively.com/v2/feeds/' + settings.id + '?key=' + settings.key;
+
+// WOW wants pressure in inches of mercury. 
+// hPa from both would be tidier.  Whatever... 
+function converthPaToHgInch( pa ){
+    var hgin = pa * 0.0295299830714;
+    return hgin.toPrecision(4);
+}
+
+// NB this function takes SLP in hPa and altitude in metres and return MSLP.
+// Approximate, but should be good for typical UK ground surface. 
+function slpToMslp( slp, altitude ){
+    var mslp = slp / Math.pow((1.0 - altitude * 2.25577e-5),5.25588); 
+    //console.log('MSLP ' + mslp);
+    return mslp;
+}
+
+// AirPi reports in Pa
+function convertSLPToHgInMSLP( pa ){
+    var slp = pa * 0.01;
+    return converthPaToHgInch( slpToMslp( slp, settings.altitude ) );
+}
+
+function processDatastream( s ){
+    var at = dateUTC( new Date( s.at ) );
+    switch( s.id ){
+    case '4':
+	// Pressure.  Report MSLP in inches of mercury.
+	// Requires adjustment for altitude. 
+	return({'baromin':convertSLPToHgInMSLP( s.current_value ),'dateutc':at});
+	break;
+    default:
+	return(null)
+    }
+}
+
+
+
+function wowUpload(ob){
+    ob.siteid=settings.siteid;
+    ob.siteAuthenticationKey=settings.siteAuthenticationKey; 
+    var query = querystring.stringify( ob );
+    console.log('WOW query ' + query); 
+    var wowURL='http://wow.metoffice.gov.uk/automaticreading?' + query;
+    http.get(wowURL, function(res) {
+	    res.on('end', function() {
+		    console.log( "WOW upload done.");
+		});
+	}).on('error', function(e) {
+		console.log("Got error: ", e);
+	    });
+}
+
+// Put your feed ID and API key in the file settings.js
+var xivelyURL='https://api.xively.com/v2/feeds/' + settings.id + '?key=' + settings.key;
 
 function getData(){
     https.get(xivelyURL, function(res) {
@@ -36,9 +91,16 @@ function getData(){
 		var response = JSON.parse(body);
 		//console.log("Got response: ", response.datastreams);
 		for( ds in response.datastreams ){
-		    console.log( response.datastreams[ds].id );
-		    console.log( response.datastreams[ds].current_value );
-		    console.log( response.datastreams[ds].at );
+		    var ob =  processDatastream( response.datastreams[ds] );
+		    if( ob != null){
+			if( settings.wow ){
+			    wowUpload( ob ); 
+			}
+			console.log( ob );
+		    }
+		    //console.log( response.datastreams[ds].id );
+		    //console.log( response.datastreams[ds].current_value );
+		    //console.log( response.datastreams[ds].at );
 		}
 	    });
     }).on('error', function(e) {
@@ -46,10 +108,10 @@ function getData(){
 	});
 }
 
-var interval = 10 * 1000; // in miliseconds
+var interval = settings.interval * 1000; // in miliseconds
 
 var timerLoop = function() {
-    var dateutc = dateUTC();
+    var dateutc = dateUTC( new Date() );
     console.log( "timerLoop @ " + dateutc );
     getData();
     setTimeout( timerLoop, interval );
